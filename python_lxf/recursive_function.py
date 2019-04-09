225a0f7f9ee869158a1fa29a784e50dfa84e3db0
ljk <ljk13572@163.com>
2019-04-09 11:41:33 +0800

    递归函数代码提交2+详细说明

--- python_lxf/recursive_function.py
+++ python_lxf/recursive_function.py
@@ -1,12 +1,15 @@
 # 递归函数的使用
 
 # 汉诺塔的移动
+#
 
-def move(n, a, b, c):
+# 无论多少个圆块，可以抽象成为同一套思路：就是想办法把(n-1)个a柱上的圆块先移动到b柱，然后把最底部最大的一个圆块移动到c柱，最后把b柱上的(n-1)个圆块移动到c柱
+def hanoi(n, a, buffer, c):
     if n == 1:
-        print(a,'-->',c)
+        print(a, '--->', c) # 定义从a柱移动到c柱的操作
     else:
-        move(n-1,a,c,b)
-        move(1, a, b, c)
-        move(n-1,b,c,a)
-move(3,'a','b','c')
+        hanoi(n-1, a, c, buffer) # 把(n-1)个a柱上的圆块移动到缓冲区buffer柱
+        hanoi(1, a, buffer, c) # 把最底部的最大的圆块移动到c柱
+        hanoi(n-1, buffer, a, c) # 把(n-1)个缓冲区buffer柱上的圆块移动到c柱
+
+hanoi(3, 'A', 'B', 'C')
